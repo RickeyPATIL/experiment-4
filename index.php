20b5d8caea2a1d35d3a7d96ebd5abc5749d36e93
RickeyPATIL <122875134+RickeyPATIL@users.noreply.github.com>
2023-04-11 11:39:53 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Rickey Patil's Page</title>
+	<style>
+		.color-block {
+			width: 100px;
+			height: 100px;
+			margin: 10px;
+			display: inline-block;
+		}
+
+		#red {
+			background-color: red;
+		}
+
+		#green {
+			background-color: green;
+		}
+
+		#blue {
+			background-color: blue;
+		}
+
+		#purple {
+			background-color: purple;
+		}
+
+		#yellow {
+			background-color: yellow;
+		}
+	</style>
+</head>
+<body>
+	<h1>Welcome to Rickey Patil's Page</h1>
+	<p>Here are some color blocks:</p>
+	<div id="red" class="color-block"></div>
+	<div id="green" class="color-block"></div>
+	<div id="blue" class="color-block"></div>
+	<div id="purple" class="color-block"></div>
+	<div id="yellow" class="color-block"></div>
+</body>
+</html>
